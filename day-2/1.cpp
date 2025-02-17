59e9e124a4762ea86c519f09664fb527386fb2aa
krish3199 <krishvadodara99@gmail.com>
2025-02-17 15:13:04 +0530

    kri

---
+++ day-2/1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int Kinjal=10;
+    int Riya=20;
+    int Ansika;
+
+
+    cout << Kinjal << endl ;
+    cout << Riya << endl ;
+
+    cout << "---------------------------------------------"<< endl ;
+
+    Ansika = Kinjal;
+    Kinjal = Riya;
+    Riya = Ansika;
+
+    cout << Kinjal << endl ;
+    cout << Riya << endl ;
+
+}
---
+++ day-2/2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int Kinjal=10;
+    int Riya=20;
+   
+
+
+    cout << Kinjal << endl ;
+    cout << Riya << endl ;
+
+    cout << "---------------------------------------------"<< endl ;
+
+    Kinjal = Kinjal + Riya ; //30
+    Riya = Kinjal - Riya ;//10
+    Kinjal = Kinjal - Riya ; //20
+
+    cout << Kinjal << endl ;
+    cout << Riya << endl ;
+
+}
