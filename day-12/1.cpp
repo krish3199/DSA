c8e1d2f981a3a77c39a1755835f3c0cadc346477
krish3199 <krishvadodara99@gmail.com>
2025-04-01 15:55:13 +0530

    project

--- day-12/1.cpp
+++ day-12/1.cpp
@@ -1,10 +1,7 @@
-// Q.4 Sum of Elements in Row & Column of 2D Array //
-
 #include <iostream>
 using namespace std;
 
-int main()
-{
+int main() {
     int row, col;
 
     cout << "Enter the number of rows: ";
@@ -14,47 +11,19 @@ int main()
 
     int box[row][col];
 
-    for (int i = 0; i < row; i++)
-    {
-        for (int j = 0; j < col; j++)
-        {
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {  
             cout << "box[" << i << "][" << j << "] : ";
             cin >> box[i][j];
         }
     }
 
-    for (int i = 0; i < row; i++)
-    {
-        for (int j = 0; j < col; j++)
-        {
-            cout << box[i][j] << " ";
+    for (int i = 0; i < row; i++) {
+        int rowSum = 0;
+        for (int j = 0; j < col; j++) {  
+            rowSum += box[i][j];
         }
-        cout << endl;
+        cout << "Sum of row " << i + 1 << " = " << rowSum << endl;
     }
 
-    int rowNum;
-    cout << "Enter row number: ";
-    cin >> rowNum;
-
-    int rowSum = 0;
-    cout << "Elements of row " << rowNum << ": ";
-    for (int i = 0; i < row; i++)
-    {
-        cout << box[rowNum][i] << " ";
-        rowSum += box[rowNum][i];
-    }
-    cout << "Sum of row " << rowNum << " = " << rowSum << endl;
-
-    int colNum;
-    cout << "Enter column number: ";
-    cin >> colNum;
-
-    int colSum = 0;
-    cout << "Elements of column " << colNum << ": ";
-    for (int i = 0; i < row; i++)
-    {
-        cout << box[i][colNum] << " ";
-        colSum += box[i][colNum];
-    }
-    cout << "Sum of column " << colNum << " = " << colSum << endl;
-}
+}  
--- project-4/4.cpp
+++ project-4/4.cpp
@@ -1,22 +1,60 @@
 // Q.4 Sum of Elements in Row & Column of 2D Array //
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
-    int row , col ;
+    int row, col;
 
-    cout<<"Enter the number of rows : ";
-    cin>> row ;
-    cout<<"Enter the number of cols : ";
-    cin>> col ;
+    cout << "Enter the number of rows: ";
+    cin >> row;
+    cout << "Enter the number of columns: ";
+    cin >> col;
 
-    int arr[row][col] ;
+    int box[row][col];
 
     for (int i = 0; i < row; i++)
     {
-        
+        for (int j = 0; j < col; j++)
+        {
+            cout << "box[" << i << "][" << j << "] : ";
+            cin >> box[i][j];
+        }
     }
-    
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            cout << box[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    int rowNum;
+    cout << "Enter row number: ";
+    cin >> rowNum;
+
+    int rowSum = 0;
+    cout << "Elements of row " << rowNum << ": ";
+    for (int i = 0; i < row; i++)
+    {
+        cout << box[rowNum][i] << " ";
+        rowSum += box[rowNum][i];
+    }
+    cout << "Sum of row " << rowNum << " = " << rowSum << endl;
+
+    int colNum;
+    cout << "Enter column number: ";
+    cin >> colNum;
+
+    int colSum = 0;
+    cout << "Elements of column " << colNum << ": ";
+    for (int i = 0; i < row; i++)
+    {
+        cout << box[i][colNum] << " ";
+        colSum += box[i][colNum];
+    }
+    cout << "Sum of column " << colNum << " = " << colSum << endl;
 }
